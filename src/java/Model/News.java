6b5e1975ad0c150b990aea5e55f2b5ee5c2acd15
LongHE173420 <162280638+LongHE173420@users.noreply.github.com>
2024-06-11 17:21:04 +0700

    News
    get News form database

--- src/java/Model/News.java
+++ src/java/Model/News.java
@@ -7,6 +7,12 @@ package Model;
 import java.time.LocalDate;
 import java.util.Date;
 import java.sql.*;
+import dal.DBContext;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -14,34 +20,105 @@ import java.sql.*;
  */
 public class News {
 
-    private int newsID;
-    private int accountID;
-    private String title;
-    private String description;
-    private String img;
-    private String author;
-    private Date date;
+    private int id, accountID, groupId;
+    private String title, heading, author, image;
+    private int view, stt;
+    private String link;
+    private String createAt, updateAt;
+    private String content, groupName, accountName;
+    private Connection cnn;
+    Statement stm;
+    PreparedStatement pstm;
+    ResultSet rs;
 
-    // Getters and Setters
+    private void connect() {
+        try {
+            cnn = (new DBContext().connection);
+            if (cnn == null) {
+                System.out.println("Connect News Fail!");
+            }
+
+        } catch (Exception e) {
+
+        }
+    }
+
     public News() {
     }
 
-    public News(int newsID, int accountID, String title, String description, String img, String author, Date date) {
-        this.newsID = newsID;
+    public News(int id) {
+        this.id = id;
+    }
+
+    public News(int id, int accountID, int groupId, String title, String heading, String author, String image, int view, int stt, String link, String createAt, String updateAt, String content, String groupName, String accountName) {
+        this.id = id;
         this.accountID = accountID;
+        this.groupId = groupId;
         this.title = title;
-        this.description = description;
-        this.img = img;
+        this.heading = heading;
         this.author = author;
-        this.date = date;
+        this.image = image;
+        this.view = view;
+        this.stt = stt;
+        this.link = link;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+        this.content = content;
+        this.groupName = groupName;
+        this.accountName = accountName;
     }
 
-    public int getNewsID() {
-        return newsID;
+    public News(int id, int accountID, int groupId, String title, String heading, String author, String image, int view, String createAt, String updateAt, String content, String groupName, String accountName, int STT) {
+        this.id = id;
+        this.accountID = accountID;
+        this.groupId = groupId;
+        this.title = title;
+        this.heading = heading;
+        this.author = author;
+        this.image = image;
+        this.view = view;
+        this.stt = STT;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+        this.content = content;
+        this.groupName = groupName;
+        this.accountName = accountName;
     }
 
-    public void setNewsID(int newsID) {
-        this.newsID = newsID;
+    public News(int id, int accountID, int groupId, String title, String heading, String author, String image, int view, String createAt, String content, String groupName, String accountName) {
+        this.id = id;
+        this.accountID = accountID;
+        this.groupId = groupId;
+        this.title = title;
+        this.heading = heading;
+        this.author = author;
+        this.image = image;
+        this.view = view;
+        this.createAt = createAt;
+        this.content = content;
+        this.groupName = groupName;
+        this.accountName = accountName;
+    }
+
+    public News(int id, int accountID, int groupId, String title, String image, int stt, String link, String createAt, String updateAt, String content) {
+        this.id = id;
+        this.accountID = accountID;
+        this.groupId = groupId;
+        this.title = title;
+        this.image = image;
+        this.stt = stt;
+        this.link = link;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public int getAccountID() {
@@ -52,6 +129,14 @@ public class News {
         this.accountID = accountID;
     }
 
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
     public String getTitle() {
         return title;
     }
@@ -60,20 +145,12 @@ public class News {
         this.title = title;
     }
 
-    public String getDescription() {
-        return description;
+    public String getHeading() {
+        return heading;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
+    public void setHeading(String heading) {
+        this.heading = heading;
     }
 
     public String getAuthor() {
@@ -84,14 +161,126 @@ public class News {
         this.author = author;
     }
 
-    public Date getDate() {
-        return date;
+    public String getImage() {
+        return image;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setImage(String image) {
+        this.image = image;
     }
 
-    
+    public int getView() {
+        return view;
+    }
+
+    public void setView(int view) {
+        this.view = view;
+    }
+
+    public int getStt() {
+        return stt;
+    }
+
+    public void setStt(int stt) {
+        this.stt = stt;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(String updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public List<News> getListNews() {
+        List<News> data = new ArrayList<News>();
+        try {
+            connect();
+            String strSelect = "select n.NewsID, n.accountID, n.groupId, n.title, n.heading, n.author, n.[image], n.[view], n.createdAt, n.updatedAt,\n"
+                    + "n.content, ng.[name], (a.firstName + ' ' + a.lastName) as adminName from News n join [Account] a on a.AccountID = n.accountID\n"
+                    + "join NewsGroup ng on n.groupId = ng.id where ng.[type] = 'news'";
+            stm = cnn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            rs = stm.executeQuery(strSelect);
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                int accountId = rs.getInt(2);
+                int groupId = rs.getInt(3);
+                String title = rs.getString(4);
+                String heading = rs.getString(5);
+                String author = rs.getString(6);
+                String image = rs.getString(7);
+                int view = rs.getInt(8);
+                String createAt = convertDateTimeFormat(rs.getString(9));
+                String updateAt = convertDateTimeFormat(rs.getString(10));
+                String content = rs.getString(11);
+                String groupName = rs.getString(12);
+                String accountName = rs.getString(13);
+                data.add(new News(id, accountId, groupId, title, heading, author, image, view, createAt, content, groupName, accountName));
+            }
+
+            cnn.close();
+
+        } catch (SQLException e) {
+            System.out.println("getListNews" + e.getMessage());
+        }
+        return data;
+    }
+
+    public String convertDateTimeFormat(String inputDateTime) {
+        if (inputDateTime == null) {
+            return null;
+        } else {
+            DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            DateFormat outputFormat = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
+            try {
+                java.util.Date date = inputFormat.parse(inputDateTime);
+                return outputFormat.format(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
 
 }
---
+++ src/java/controller/discountController.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller;
+
+import Model.Discount;
+import Model.News;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Dell
+ */
+public class discountController extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet discountController</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet discountController at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    public int calThePage(int sizePage,int typeId,String search){
+        Discount d=new Discount();
+        News n =new News();
+        int pages=0;
+        int countDiscounts=0;
+        if(typeId!=-1){
+            
+        }
+    }
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
