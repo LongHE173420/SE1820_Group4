2ce8b898acb8bd09fe3d5cb51c2d8632b18424b4
LongHE173420 <162280638+LongHE173420@users.noreply.github.com>
2024-06-12 00:04:03 +0700

    News
    getNews from database

--- src/java/Model/News.java
+++ src/java/Model/News.java
@@ -85,7 +85,7 @@ public class News {
         this.accountName = accountName;
     }
 
-    public News(int id, int accountID, int groupId, String title, String heading, String author, String image, int view, String createAt, String content, String groupName, String accountName) {
+    public News(int id, int accountID, int groupId, String title, String heading, String author, String image, int view, String createAt,String updateAt, String content, String groupName, String accountName) {
         this.id = id;
         this.accountID = accountID;
         this.groupId = groupId;
@@ -95,6 +95,7 @@ public class News {
         this.image = image;
         this.view = view;
         this.createAt = createAt;
+        this.updateAt=updateAt;
         this.content = content;
         this.groupName = groupName;
         this.accountName = accountName;
@@ -237,7 +238,7 @@ public class News {
         List<News> data = new ArrayList<News>();
         try {
             connect();
-            String strSelect = "select n.NewsID, n.accountID, n.groupId, n.title, n.heading, n.author, n.[image], n.[view], n.createdAt, n.updatedAt,\n"
+            String strSelect = "select n.id, n.accountID, n.groupId, n.title, n.heading, n.author, n.[image], n.[view], n.createdAt, n.updatedAt,\n"
                     + "n.content, ng.[name], (a.firstName + ' ' + a.lastName) as adminName from News n join [Account] a on a.AccountID = n.accountID\n"
                     + "join NewsGroup ng on n.groupId = ng.id where ng.[type] = 'news'";
             stm = cnn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
@@ -265,6 +266,296 @@ public class News {
             System.out.println("getListNews" + e.getMessage());
         }
         return data;
+    }
+    
+    public News getNewsById(int nId){
+        try{
+            connect();
+            String selectStr="select n.id, n.accountID, n.groupId,n.title, n.heading, n.author, n.[image],n.createdAt, n.updatedAt, n.content, ng.[name], \n"
+                    + "(na.firstName + ' ' + na.lastName) as adminName, n.[view] from news n join NewsGroup ng on n.groupId = ng.id \n"
+                    + "join [Account] na on n.accountID = na.AccountID where n.id = ?";
+            pstm=cnn.prepareStatement(selectStr);
+            pstm.setInt(1, nId);
+            rs=pstm.executeQuery();
+            if(rs.next()){
+                int id=rs.getInt(1);
+                int accountId=rs.getInt(2);
+                int groupId=rs.getInt(3);
+                String title=rs.getString(4);
+                String heading=rs.getString(5);
+                String author=rs.getString(6);
+                String image=rs.getString(7);
+                String createAt=convertDateTimeFormat(rs.getString(8));
+                String updatedAt=convertDateTimeFormat(rs.getString(9));
+                String content=rs.getString(10);
+                String groupName=rs.getString(11);
+                String accountName=rs.getString(12);
+                int view=rs.getInt(13);
+                return new News(id, accountId, groupId, title, heading, author, image, view, createAt, updateAt, content, groupName, accountName);
+            }
+            cnn.close();
+        }catch(SQLException e){
+            System.out.println("getNewsById: "+e.getMessage());
+        }
+        return null;
+    }
+    public List<News> getNewsByGid(int gid){
+        List<News> data =new ArrayList<News>();
+        try{
+            connect();
+            String selectStr= "select n.id, n.accountID, n.groupId,n.title, n.heading, n.author, n.[image],n.createdAt, n.updatedAt, n.content, ng.[name], \n"
+                    + "(na.firstName + ' ' + na.lastName) as adminName, n.[view] from news n join NewsGroup ng on n.groupId = ng.id \n"
+                    + "join [Account] na on n.accountID = na.AccountID where n.groupId = ? order by n.createdAt";
+            pstm=cnn.prepareStatement(selectStr);
+            pstm.setInt(1, gid);
+            rs=pstm.executeQuery();
+            while(rs.next()){
+                int id=rs.getInt(1);
+                int accountId=rs.getInt(2);
+                int groupId=rs.getInt(3);
+                String title=rs.getString(4);
+                String heading=rs.getString(5);
+                String auhtor=rs.getString(6);
+                String image=rs.getString(7);
+                String createAt=convertDateTimeFormat(rs.getString(8));
+                String updatedAt=convertDateTimeFormat(rs.getString(9));
+                String content=rs.getString(10);
+                String groupName=rs.getString(11);
+                String accountName=rs.getString(12);
+                int view=rs.getInt(13);
+                data.add(new News(id, accountId, groupId, title, heading, author, image, view, createAt, updateAt, content, groupName, accountName));
+            }
+            cnn.close();
+        }catch(SQLException e){
+            System.out.println("getListNewsByGid: "+e.getMessage());
+        }
+        return data;
+    }
+    public List<News> getListNewsAndSearch(String search){
+        List<News> data=new ArrayList<News>();
+        try{
+            connect();
+            String strSelect="select n.id, n.accountID, n.groupId, n.title, n.heading, n.author, n.[image], n.[view], n.createdAt, n.updatedAt,\n"
+                    + "n.content, ng.[name], (a.firstName + ' ' + a.lastName) as adminName from news n join [Account] a on a.AccountID = n.accountID\n"
+                    + "join NewsGroup ng on n.groupId = ng.id where ng.[type] = 'news' and (n.title like '%" + search + "%' or n.author like '%" + search + "%')";
+            pstm=cnn.prepareStatement(strSelect);
+            rs=pstm.executeQuery();
+            while(rs.next()){
+                int id=rs.getInt(1);
+                int accountId=rs.getInt(2);
+                int groupId=rs.getInt(3);
+                String title=rs.getString(4);
+                String heading=rs.getString(5);
+                String author=rs.getString(6);
+                String image=rs.getString(7);
+                int view=rs.getInt(8);
+                String createAt=convertDateTimeFormat(rs.getString(9));
+                String updateAt=convertDateTimeFormat(rs.getString(10));
+                String content=rs.getString(11);
+                String groupName=rs.getString(12);
+                String accountName=rs.getString(13);
+                data.add(new News(id, accountId, groupId, title, heading, author, image, view, createAt, updateAt, content, groupName, accountName));
+            }
+            cnn.close();
+        }catch(SQLException e){
+            System.out.println("getListNewsAndSearch 1 param: "+e.getMessage());
+        }
+        return data;
+    }
+    public List<News> getNewsByGidAndSearch(int gid,String search){
+        List<News> data=new ArrayList<News>();
+        try{
+            connect();
+            String selectStr="select n.id, n.accountID, n.groupId,n.title, n.heading, n.author, n.[image],n.createdAt, n.updatedAt, n.content, ng.[name], \n"
+                    + "(na.firstName + ' ' + na.lastName) as adminName, n.[view] from News n join NewsGroup ng on n.groupId = ng.id \n"
+                    + "join [Account] na on n.accountID = na.AccountID where n.groupId = ? and (n.title like '%" + search + "%' or n.author like '%" + search + "%')";
+            pstm=cnn.prepareStatement(selectStr);
+            pstm.setInt(1, gid);
+            rs=pstm.executeQuery();
+            while(rs.next()){
+                int id=rs.getInt(1);
+                int accountId=rs.getInt(2);
+                int groupId=rs.getInt(3);
+                String title=rs.getString(4);
+                String heading=rs.getString(5);
+                String author=rs.getString(6);
+                String image=rs.getString(7);
+                String createAt=rs.getString(8);
+                String updatedAt=rs.getString(9);
+                String content=rs.getString(10);
+                String groupName=rs.getString(11);
+                String accountName=rs.getString(12);
+                int view=rs.getInt(13);
+                data.add(new News(id, accountId, groupId, title, heading, author, image, view, createAt, updatedAt, content, groupName, accountName));
+            }
+            cnn.close();
+        }catch(SQLException e){
+            System.out.println("getNewsbyGidAndSearch 2 params: "+e.getMessage());
+        }
+        return data;
+    }
+    public void addNews(int aid ,int gid,String title,String image,String heading,String author,String createdAt,String content){
+        try{
+            connect();
+            String strAdd="INSERT INTO [News]([accountID], [groupId], [title], [image], [heading], [author], [createdAt], [content])"
+                    + " values (?,?,?,?,?,?,?,?)";
+            pstm=cnn.prepareStatement(strAdd);
+            pstm.setInt(1, aid);
+            pstm.setInt(2, gid);
+            pstm.setString(3, title);
+            pstm.setString(4, image);
+            pstm.setString(5, heading);
+            pstm.setString(6, author);
+            pstm.setString(7, createdAt);
+            pstm.setString(8, content);
+            pstm.execute();
+            cnn.close();
+        }catch(SQLException e){
+            System.out.println("addNews: "+e.getMessage());
+        }
+    }
+    public void updateNews(int gid,String title,String image,String heading,String author,String updatedAt,String content,int id){
+        try{
+            connect();
+            String strUpdate="UPDATE [dbo].[News]\n"
+                    + "   SET [groupId] = ?\n"
+                    + "      ,[title] = ?\n"
+                    + "      ,[image] = ?\n"
+                    + "      ,[heading] = ?\n"
+                    + "      ,[author] = ?\n"
+                    + "      ,[updatedAt] = ?\n"
+                    + "      ,[content] = ?\n"
+                    + " WHERE id = ?";
+            pstm=cnn.prepareStatement(strUpdate);
+            pstm.setInt(1, gid);
+            pstm.setString(2, title);
+            pstm.setString(3, image);
+            pstm.setString(4, heading);
+            pstm.setString(5, author);
+            pstm.setString(6, updatedAt);
+            pstm.setString(7, content);
+            pstm.setInt(8, id);
+            pstm.execute();
+            cnn.close();
+        }catch(Exception e){
+            System.out.println("updateNews: "+e.getMessage());
+        }
+    }
+    public void updatePolicy(String title,String updatedAt,String content,int id){
+        try{
+            connect();
+            String strUpdate="UPDATE [dbo].[News]\n"
+                    + "   SET [title] = ?\n"                    
+                    + "      ,[updatedAt] = ?\n"
+                    + "      ,[content] = ?\n"
+                    + " WHERE id = ?";
+            pstm=cnn.prepareStatement(strUpdate);
+            pstm.setString(1, title);
+            pstm.setString(2, updatedAt);
+            pstm.setString(3, content);
+            pstm.setInt(4, id);
+            pstm.execute();
+            cnn.close();
+            
+        }
+        catch(Exception e){
+            System.out.println("updatePolicy: "+e.getMessage());
+        }
+    }
+    public void updateFooterContent(String title,String content,String updatedAt,int id){
+        try{
+            String strUpdate="UPDATE [dbo].[News]\n"
+                    + "   SET [title] = ?\n"
+                    + "      ,[updatedAt] = ?\n"
+                    + "      ,[content] = ?\n"
+                    + " WHERE id = ?";
+            pstm=cnn.prepareStatement(strUpdate);
+            pstm.setString(1, title);
+            pstm.setString(2, updatedAt);
+            pstm.setString(3, content);
+            pstm.setInt(4, id);
+            pstm.execute();
+            cnn.close();
+        }catch(Exception e){
+            System.out.println("updateFooterContent: "+e.getMessage());
+        }
+    }
+    public void DeleteContents(String sid,String nid){
+        connect();
+        int x=1;
+        try{
+            String strSelect="delete from News where id = ?";
+            pstm=cnn.prepareStatement(strSelect);
+            pstm.setString(1, sid);
+            pstm.execute();
+        }catch(Exception e){
+            System.out.println("DeleteContent: "+e.getMessage());
+        }
+        List<Integer> data=new ArrayList<Integer>();
+        try{
+            String strSelect="select News.id from [News] where groupId = ? order by STT asc";
+            pstm=cnn.prepareStatement(strSelect);
+            pstm.setString(1, nid);
+            rs=pstm.executeQuery();
+            while(rs.next()){
+                id=rs.getInt(1);
+                data.add(id);
+            }
+        }catch(Exception e){
+            System.out.println("getListContentByGid: "+e.getMessage());
+        }
+            try{
+                for(Integer n : data){
+                    String strUpdate="UPDATE [News]\n"
+                        + "   SET [STT] = ?\n"
+                        + " WHERE id = ?";
+                    pstm=cnn.prepareStatement(strUpdate);
+                    pstm.setInt(1, x);
+                    pstm.setInt(2, n);
+                    pstm.execute();
+                    x++;
+                    
+                }
+                cnn.close();
+            }catch (Exception e){
+                System.out.println("DeleteContent: "+e.getMessage());
+            }      
+    }
+    public void DeleteNews(String sid){
+        try{
+            connect();
+            String strSelect="delete from News where id = ?";
+            pstm=cnn.prepareStatement(strSelect);
+            pstm.setString(1, sid);
+            pstm.execute();
+            cnn.close();
+        }catch(Exception e){
+            System.out.println("DeleteContent: "+e.getMessage());
+        }
+    }
+    public void isSlideBanner(String id){
+        try{
+            connect();
+            String strSelect="update [News] set [STT] = 1 where id= ?";
+            pstm=cnn.prepareStatement(strSelect);
+            pstm.setString(1, id);
+            pstm.execute();
+            cnn.close();
+        }catch(Exception e){
+            System.out.println("isSlideBanner: "+e.getMessage());
+        }
+    }
+    public void notSlideBanner(String id){
+        try{
+            connect();
+            String strSelect="update [News] set [STT] = NULL where id= ?";
+            pstm=cnn.prepareStatement(strSelect);
+            pstm.setString(1, id);
+            pstm.execute();
+            cnn.close();
+        }catch(Exception e){
+            System.out.println("notSlideBanner: "+e.getMessage());
+        }
     }
 
     public String convertDateTimeFormat(String inputDateTime) {
